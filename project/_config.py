bc9d5f2a619e24b4ba413862fcdea425e4ff7b40
Jose Cuellar <cuellar_jose@hotmail.com>
2016-07-18 00:21:44 -0700

    updated to sqlalchemy

--- project/_config.py
+++ project/_config.py
@@ -2,12 +2,16 @@ import os
 
 # grab the folder where this scrip lives
 basedir = os.path.abspath(os.path.dirname(__file__))
+# print basedir
 
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = '\x07\x95\x15(\xf5~\xac\xa0\xdd^\x96#\x97\x0ew\xfb\x8e\x08`+2\x9a\x85\x0b'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+# print DATABASE_PATH
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
