939af031b4de8abd5e4b1ad1844992a468ea7f23
Jose Cuellar <cuellar_jose@hotmail.com>
2016-08-02 21:01:05 -0700

    created fabfile

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+def test():
+	local("nosetests -v")
+
+
+def commit():
+	message = raw_input("Enter a git commit message: ")
+	local("git add . && git commit -am '{}'".format(message))
+
+
+def push():
+	local("git push origin master")
+
+
+def prepare():
+	test()
+	commit()
+	push()
